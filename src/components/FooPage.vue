f2fd208d5ebca8da6d8d226dec6b0101aa43ff57
zinc <15332538780@163.com>
2022-10-30 01:27:50 +0800

    vue路由

---
+++ src/components/FooPage.vue
@@ -0,0 +1,13 @@
+<template lang="zh">
+    <div>
+        <h1>这是foo组件</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <FooPage/>
+  </div>
+</template>
+<script>
+    import FooPage from '../components/FooPage.vue'
+export default {
+    components: {
+        FooPage
+    }
+}
+</script>
