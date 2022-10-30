299ee68b0b90f035142b3bff7f6dc249389d5345
zinc <15332538780@163.com>
2022-10-30 23:35:07 +0800

    更改

---
+++ src/views/UserView.vue
@@ -0,0 +1,15 @@
+<template lang="zh">
+    <transition name="fade">
+    <div class='user'>
+        <h1>用户ID为 {{1+1}}</h1>
+       
+    </div>
+    </transition>
+</template>
+<script>
+export default {
+ 
+};
+</script>
+<style lang="">
+</style>
